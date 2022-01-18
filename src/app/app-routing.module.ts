9d9d3b853158d921aa8f2cfe0b556ee47f3d81e2
Nomelize99 <93709755+Nomelize99@users.noreply.github.com>
2022-01-18 18:46:00 +0700

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//path
+
+import { ProductComponent } from './component/product/product.component';
+import { ManagementComponent } from './component/management/management.component';
+import { ProductlistComponent } from './component/productlist/productlist.component';
+import { ReceiptlistComponent } from './component/receiptlist/receiptlist.component';
+import { RegisterComponent } from './component/register/register.component';
+import { CatagoryComponent } from './component/catagory/catagory.component';
+import { OrderComponent } from './component/order/order.component';
+import { OrderdetailComponent } from './component/orderdetail/orderdetail.component';
+import { AuthGuard } from './auth/auth.guard';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo:'product'},
+  { path: 'register', component: RegisterComponent},
+  { path: 'product', component: ProductComponent},
+  { path: 'management', component:ManagementComponent , canActivate: [AuthGuard]},
+  { path: 'receiptlist', component: ReceiptlistComponent , canActivate: [AuthGuard]},
+  { path: 'productlist', component: ProductlistComponent , canActivate: [AuthGuard]},
+  { path: 'catagory', component: CatagoryComponent , canActivate: [AuthGuard]},
+  { path: 'orderlist', component: OrderComponent},
+  { path: 'order/user', component: OrderdetailComponent}/*
+  { path: '**', pathMatch: 'full', redirectTo:'product' }*/
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers:[AuthGuard]  
+})
+export class AppRoutingModule { }
